5f1cdfcb270df4a89e81c1847a63a4ec10dd78c8
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2020-12-08 19:58:54 +0100

    Day 1 Part 2

--- Day 1/solve.py
+++ Day 1/solve.py
@@ -7,8 +7,15 @@ print(f'Running @ {path}')
 wo = [int(line.rstrip('\n')) for line in open(f'{path}/input.txt', 'r')]
 wo.sort()
 
-
 target = 2020
+print(f'Finding the product of the two numbers that sum to {target}')
 for el in wo:
     if (2020-el in wo):
         print(f'Found {el}; {el*(2020-el)}')
+
+print(f'Finding the product of the three numbers that sum to {target}')
+n = len(wo)
+for k in range(0, n):
+    for j in range(k, n):
+        if target - wo[k] - wo[j] in wo:
+            print(f'Found {target-wo[k]-wo[j]} {wo[j]} {wo[k]} : {(target-wo[k]-wo[j])*wo[j]*wo[k]}')
