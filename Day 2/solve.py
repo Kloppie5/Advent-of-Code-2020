e80d57505823307f419b4a6c95c53d2e73a98dd9
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2020-12-12 14:58:06 +0100

    Day 2 Part 2

--- Day 2/solve.py
+++ Day 2/solve.py
@@ -4,12 +4,17 @@ import os
 path = os.path.dirname(os.path.abspath(__file__))
 print(f'Running @ {path}')
 
-c = 0
+cs = 0
+ct = 0
 for line in open(f'{path}/input.txt', 'r'):
     data = line.rstrip('\n').split(' ')
-    data = {'min': int(data[0].split('-')[0]), 'max': int(data[0].split('-')[1]), 'letter': data[1].split(':')[0], 'password': data[2]}
+    data = {'a': int(data[0].split('-')[0]), 'b': int(data[0].split('-')[1]), 'letter': data[1].split(':')[0], 'password': data[2]}
     occ = data['password'].count(data['letter'])
-    if data['min'] <= occ and occ <= data['max']:
-        # print(f"{data} is valid")
-        c = c + 1
-print(c)
+    if data['a'] <= occ and occ <= data['b']:
+        print(f"{data} is valid following the sled requirements")
+        cs = cs + 1
+    if bool(data['password'][data['a']-1] == data['letter']) ^ bool(data['password'][data['b']-1] == data['letter']):
+        print(f"{data} is valid following the toboggan requirements")
+        ct = ct + 1
+print(f'{cs} valid passwords following the sled requirements')
+print(f'{ct} valid passwords following the toboggan requirements')
