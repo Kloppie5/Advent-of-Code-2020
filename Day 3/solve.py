ed7c2f2e7478222c34869dc816477e3da1b2d1ef
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2020-12-12 15:15:22 +0100

    Day 3 Part 2

--- Day 3/solve.py
+++ Day 3/solve.py
@@ -6,20 +6,31 @@ print(f'Running @ {path}')
 
 data = [list(line.rstrip('\n')) for line in open(f'{path}/input.txt', 'r')]
 
-height = len(data)
-width = len(data[0])
+def treecount (data, dx, dy) :
+    height = len(data)
+    width = len(data[0])
+    
+    posx = 0
+    posy = 0
+    
+    c = 0
+    while posy < height:
+        if data[posy][posx] == '#':
+            c = c + 1
+        posx = (posx + dx) % width
+        posy = posy + dy
+    return c
 
-right = 3
-down = 1
+t11 = treecount(data, 1, 1)
+print(f'({1, 1}): {t11}')
+t31 = treecount(data, 3, 1)
+print(f'({3, 1}): {t31}')
+t51 = treecount(data, 5, 1)
+print(f'({5, 1}): {t51}')
+t71 = treecount(data, 7, 1)
+print(f'({7, 1}): {t71}')
+t12 = treecount(data, 1, 2)
+print(f'({1, 2}): {t12}')
 
-posx = 0
-posy = 0
-
-c = 0
-while posy < height:
-    print(f'({posx}, {posy}): {data[posy][posx]}')
-    if data[posy][posx] == '#':
-        c = c + 1
-    posx = (posx + right) % width
-    posy = posy + down
-print(c)
+print(f'Total trees: {t11} {t31} {t51} {t71} {t12}')
+print(f'Total "score": {t11*t31*t51*t71*t12}')
