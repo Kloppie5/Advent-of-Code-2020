ffd653923d9fdd12974a9d130b159ac6a55e36fc
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2020-12-13 17:41:49 +0100

    Day 5 Part 2

--- Day 5/solve.py
+++ Day 5/solve.py
@@ -4,10 +4,14 @@ import os
 path = os.path.dirname(os.path.abspath(__file__))
 print(f'Running @ {path}')
 
-max = 0
+ids = []
 for line in open(f'{path}/input.txt', 'r').read().split('\n'):
     id = int(line.replace('F', '0').replace('B', '1').replace('R', '1').replace('L', '0'), 2)
-    if id > max:
-        max = id
-print(max)
+    ids.append(id)
+ids.sort()
 
+prev = ids[0]
+for id in ids:
+    if id == prev + 2:
+        print(id-1)
+    prev = id
